eef70f1fa8b1a9cb4afb5b7853b940e96e457828
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-07-15 00:52:57 +0700

    fix name path

---
+++ src/test/java/com/nashtech/demoqa/tests/BaseTest.java
@@ -0,0 +1,30 @@
+package com.nashtech.demoqa.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+    public WebDriver driver;
+
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void BeforeMethod() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("");
+
+    }
+
+    @AfterMethod
+    public void AfterMethod() {
+        driver.quit();
+    }
+}
