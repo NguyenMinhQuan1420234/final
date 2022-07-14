d6e38e119d193b1e57734011b377abd3dd793714
Minh Quan <banchi0072000@gmail.com>
2022-07-14 22:29:33 +0700

    first commit

---
+++ src/test/java/com/nashtech/quannguyen/pages/BasePage.java
@@ -0,0 +1,11 @@
+package com.nashtech.quannguyen.pages;
+
+public class BasePage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration)
+    }
+}
